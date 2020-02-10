89886ed0340e7ba326cabcc2cb71d14f65c3c28f
dmaksiuta <shutle369@gmail.com>
2020-02-10 23:10:29 +0200

    changed structure

--- Landing Page/README.md
+++
@@ -1,4 +0,0 @@
-# Start
- "npm i" for install node modules
- "go" in terminal or npm run start
-
--- Landing Page/src/app/waw.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(e){
-   $('.drop-wrap').on('click',function(){
-      $('.drop-wrap ul').slideToggle();
-   });
-})
